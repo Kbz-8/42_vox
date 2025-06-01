181a8647d6640a3ab9c778b21ea1f9c203dac8c6
Kbz-8 <kbz_8.dev@akel-engine.com>
2025-06-01 16:52:01 +0200

    adding post process and AO

--- ScopEngine/Script/FetchDependencies.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Update Vulkan headers
-rm -rf ThirdParty/vulkan
-wget https://github.com/KhronosGroup/Vulkan-Headers/archive/main.zip -O vulkan-headers.zip
-unzip -o vulkan-headers.zip -d ThirdParty/
-mv ThirdParty/Vulkan-Headers-main/include/vulkan ThirdParty/
-rm -rf ThirdParty/Vulkan-Headers-main
-rm vulkan-headers.zip
